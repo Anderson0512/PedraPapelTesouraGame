a0d01b40eea0f97575aedb4b5c8e5722c72d3a2e
Anderson0512 <joao_anderson26@hotmail.com>
2023-10-08 23:23:48 -0300

    boas prãticas

--- src/main/java/org/game/model/gameException/GameException.java
+++ src/main/java/org/game/model/gameException/GameException.java
@@ -1,9 +1,11 @@
 package org.game.model.gameException;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class GameException extends RuntimeException implements Serializable {
-    public static final long serialVersionUID = 1L;
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public GameException(String message) {
         super(message);
